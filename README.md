90bc35a130b093fee6b583d7b264babb0ffc15aa
Tran Trung Tin <trungtindev@gmail.com>
2017-03-02 04:08:08 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# bkk-app
+
+This is a prototype.
+
+## Install
+
+```sh
+npm install
+```
+
+## Run application
+
+```sh
+npm start
+```
---
+++ src/renderer/components/main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+export default class Main extends React.Component {
+  constructor () {
+    super()
+  }
+
+  render () {
+    return <div>
+      <p>Hello world</p>
+      <img src='../assets/images/electron.svg' alt='' />
+    </div>
+  }
+}
