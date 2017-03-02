1d89369c1135c4b5b8ee72d5cc1f9e3a8ef19f11
Tran Trung Tin <trungtindev@gmail.com>
2017-03-02 10:23:27 +0700

    add blueprint

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 
+import { FocusStyleManager } from '@blueprintjs/core'
+
+FocusStyleManager.onlyShowFocusOnTabs()
+
 const render = () => {
   // NB: We have to re-require MyApp every time or else this won't work
   // We also need to wrap our app in the AppContainer class
-  const Main = require('./components/main').default
+  const Main = require('./containers/main').default
   ReactDOM.render(<AppContainer><Main /></AppContainer>, document.getElementById('app'))
 }
 
