5afdd1dd21821682268626c11848e287d9618a84
Tran Trung Tin <trungtindev@gmail.com>
2017-03-03 21:04:00 +0700

    wip

--- src/renderer/containers/Setting/Setting.js
+++ src/renderer/containers/Setting/Setting.js
@@ -1,6 +1,9 @@
 /* @flow */
 
 import React from 'react'
+import { remote } from 'electron'
+
+const { dialog } = remote
 
 type SettingProps = {
 
@@ -18,10 +21,15 @@ export default class Setting extends React.Component {
       <div className='container-fluid'>
         <div className='pt-control-group'>
           <div className='pt-input-group'>
-            <span className='pt-icon pt-icon-people' />
+            <span className='pt-icon pt-icon-folder-open' />
             <input type='text' className='pt-input' style={{ paddingRight: '94px' }} disabled />
           </div>
-          <button className='pt-button pt-intent-primary'>Choose</button>
+          <button className='pt-button pt-intent-primary' onClick={() => dialog.showOpenDialog({
+            properties: ['openDirectory', 'createDirectory', 'promptToCreate'],
+            title: 'Select storage folder'
+          }, function (f) {
+            console.log(f)
+          })} >Choose</button>
         </div>
       </div>
     )
