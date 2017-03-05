327246fe5dbe1d8d1b153dda96791dff24fb222c
Tran Trung Tin <trungtindev@gmail.com>
2017-03-05 11:38:52 +0700

    update epics (wip)

--- src/renderer/epics/index.js
+++ src/renderer/epics/index.js
@@ -1,4 +1,4 @@
 import { combineEpics } from 'redux-observable'
 import * as indexingEpics from './indexing'
 
-export default combineEpics(...Object.values(indexingEpics))
+export default combineEpics(...Object.keys(indexingEpics).map(k => indexingEpics[k]))
--- src/renderer/epics/indexing.js
+++ src/renderer/epics/indexing.js
@@ -1,7 +1,70 @@
-import { TYPES } from '../redux/actions'
+import { TYPES, addBookPath } from '../redux/actions'
+import fs from 'fs'
+import path from 'path'
+import Rx from 'rxjs'
 
-export function addLibraryPath (action$, store) {
+import { get } from '../storage/db'
+
+const ALLOWED_EXTENSIONS = ['.epub']
+
+function resolve (dir) {
+  return function (file) {
+    return path.resolve(dir, file)
+  }
+}
+
+const readdirFullPath = function (dir, cb) {
+  fs.readdir(dir, function (err, files) {
+    cb(err, files && files.map(resolve(dir)))
+  })
+}
+
+const readDirAsObservable = Rx.Observable.bindNodeCallback(readdirFullPath)
+
+export function watchAddLibraryPath (action$, store) {
   return action$.ofType(TYPES.ADD_LIBRARY_PATH)
-    .map(action => action.payload.path)
-    .filter(p => !!p)
+    // pluck the path (array of path actually) and filter empty one
+    .map(action => action.payload.path.filter(p => p.length))
+    // Now filter the empty array
+    .filter(pathArray => {
+      return Array.isArray(pathArray) && !!pathArray.length
+    })
+    // And readdir as an observable, then merge into the same stream
+    .mergeMap(pathArray => {
+      return Rx.Observable.merge(...pathArray.map(p => readDirAsObservable(p)))
+    })
+    // Then flatten the array
+    .mergeMap(x => x)
+    // Recusively travel the path
+    .expand(filePath => {
+      // Break point when catch the allowed file
+      if (typeof filePath !== 'string') {
+        return Rx.Observable.empty()
+      }
+      const stat = fs.statSync(filePath)
+      if (stat.isDirectory()) {
+        return readDirAsObservable(filePath).mergeMap(x => x)
+      } else {
+        const fileExt = path.extname(filePath)
+        if (~ALLOWED_EXTENSIONS.indexOf(fileExt.toLowerCase())) {
+          return Rx.Observable.of({
+            path: filePath,
+            ext: fileExt
+          })
+        }
+        return Rx.Observable.empty()
+      }
+    })
+    .filter(p => typeof p === 'object')
+    .map(p => addBookPath(p))
+}
+
+export function watchAddBookPath (action$, store) {
+  return Rx.Observable.combineLatest(
+    Rx.Observable.fromPromise(get()),
+    action$.ofType(TYPES.ADD_BOOK_PATH)
+  )
+    .map((db, payload) => {
+      return Rx.Observable.of(db, payload)
+    })
 }
